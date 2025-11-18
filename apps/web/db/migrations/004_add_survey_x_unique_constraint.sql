8fc84c3fed2b3fc5fca7f5a3bcfb960e0d3af065
therealityreport <admin@thereality.report>
2025-11-17 23:52:48 -0500

    tweak: refine hub header layout

--- apps/web/db/migrations/004_add_survey_x_unique_constraint.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-
--- Add UNIQUE constraint on app_user_id for survey_x_responses
--- This is required for the ON CONFLICT clause in upsert operations
-ALTER TABLE survey_x_responses
-ADD CONSTRAINT survey_x_responses_app_user_id_key UNIQUE (app_user_id);
-
-COMMIT;
